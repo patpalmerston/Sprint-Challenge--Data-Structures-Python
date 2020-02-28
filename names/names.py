b15f082a56c70165938a5c993a5b89fa6736018a
Pat Palmerston <patpalmerston@gmail.com>
2020-02-28 09:46:19 -0800

    run time complexities for names

--- names/names.py
+++ names/names.py
@@ -20,15 +20,19 @@ duplicates = []  # Return the list of duplicates in this data structure
 # each name is a new BST in the BST
 bst = BinarySearchTree(names_1[0][1])
 # loop through the names and insert into the bst DS
-for name in names_1:
+for name in names_1:  # O(n)
     bst.insert(name)
 
 # then check if names in name_2 are contained in the bst. if so append them to list
-for name in names_2:
-    if bst.contains(name):
-        duplicates.append(name)
+for name in names_2:  # O(n)
+    if bst.contains(name):  # O(log n) bst searches for target using (log n)
+        duplicates.append(name)  # O(n) each duplicate gets appended once
 
-# effectively changing the complexity from a O(n^2) to just O(2n) = O(n)
+# I believe this would give us O(n log n) and our run time is at about .3-.5 seconds
+
+# original time/space complexity-------------------
+# complexities: time O(n^2)(two actions for every name)
+        # space O(n) amount of names remains the same in the loops and the append only appends one name for each name found that is a duplicate. So O(1000n) is still O(n)
 
 # for name_1 in names_1:
 #     for name_2 in names_2:
