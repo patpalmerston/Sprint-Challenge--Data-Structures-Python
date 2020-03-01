f7a2d0270d662e21cde44959014bcfcac5b57f34
Pat Palmerston <patpalmerston@gmail.com>
2020-03-01 13:14:47 -0800

    ring buffer passing all tests

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -11,22 +11,33 @@ class RingBuffer:
         return f"cap:{self.capacity}, curr:{self.current}, stor:{self.storage}"
 
     def append(self, item):
-        # check to see if the capacity is greater than the length of nodes in the dll
-        if self.capacity > self.storage.length:
-            # if it is  than we can add to the storage
-            # we would add to the tail(newest)
+        # current is always the oldest
+        # while storage is less than capacity
+        if self.storage.length < self.capacity:
+            # add item to tail
             self.storage.add_to_tail(item)
-            # then the head(oldest) would become current
-            self.current = self.storage.head
+            # tail becomes current(newest)
+            self.current = self.storage.tail
 
-        # elif if capacity and storage are equal we need to
-        elif self.capacity == self.storage.length:
-            # remove the head
-            self.storage.remove_from_head()
-        # add new item to the head
-            self.storage.add_to_head(item)
-        # head equals new item
-            self.storage.head = item
+            # if storage is equal to capacity
+        elif self.storage.length == self.capacity:
+
+            if self.current is self.storage.tail:
+                # remove oldest item from head
+                self.storage.remove_from_head()
+                # add newest item to head
+                self.storage.add_to_head(item)
+                # move current forward to head which is newest
+                self.current = self.storage.head
+            else:
+                # insert item after the current
+                self.current.insert_after(item)
+                # keep track of increase in storage
+                self.storage.length += 1
+                # move current forward to be the newest node
+                self.current = self.current.next
+                # delete the node in front(oldest) and delete will auto decrease length of storage
+                self.storage.delete(self.current.next)
 
     def get(self):
         # Note:  This is the only [] allowed
@@ -38,15 +49,10 @@ class RingBuffer:
         var_head = self.storage.head
         # we want to kick out of loop once there is no next, so eventual var_head will = None at the end of the loop
 
-        # var_head_value = self.storage.head.value
-
-        var_head_value = self.storage.head.value
-
         while var_head is not None:
             # append the head to the list
-            list_buffer_contents.append(var_head_value)
+            list_buffer_contents.append(var_head.value)
 
-            var_head_value = self.storage.head.next.value
         # run through the head.next
             var_head = var_head.next
         # return the list
@@ -64,3 +70,21 @@ class ArrayRingBuffer:
 
     def get(self):
         pass
+
+
+# # check to see if the capacity is greater than the length of nodes in the dll
+#         if self.capacity > self.storage.length:
+#             # if it is  than we can add to the storage
+#             # we would add to the tail(newest) until we hit capacity
+#             self.storage.add_to_tail(item)
+#             # The head(oldest) would become current
+#             self.current = self.storage.head
+
+#         # elif if capacity and storage are equal we need to
+#         elif self.capacity == self.storage.length:
+#             # remove the head
+#             self.storage.remove_from_head()
+#             # add new item to the head
+#             self.storage.add_to_head(item)
+#             # move current forward one
+#             self.current = self.current.next
